e6b1b6de284a1a3018ab446a4361389fd7c3ed7c
Jordan Moldow <jmoldow@mit.edu>
2013-12-07 17:23:41 -0500

    Adds scaffolding for REMOTE ccache

---
+++ src/lib/krb5/ccache/cc_remote.c
@@ -0,0 +1,332 @@
+#include "k5-int.h"
+#include "cc-int.h"
+
+#if HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#if HAVE_SYS_STAT_H
+#include <sys/stat.h>
+#endif
+
+#include <dirent.h>
+
+extern const krb5_cc_ops krb5_rcc_ops;
+extern const krb5_cc_ops krb5_fcc_ops;
+
+/* Fields are not modified after creation, so no lock is necessary. */
+typedef struct rcc_data_st {
+    char *residual;             /* dirname or :filename */
+    krb5_ccache fcc;            /* File cache for actual cache ops */
+} rcc_data;
+
+/* Verify that the remote exists as a socket. */
+static krb5_error_code
+verify_remote(krb5_context context, const char *sockname)
+{
+    /* TODO implement remote socket */
+    struct stat st;
+
+    if (stat(sockname, &st) < 0) {
+        krb5_set_error_message(context, KRB5_FCC_NOFILE,
+                               "Socket %s does not "
+                               "exist", sockname);
+        return KRB5_FCC_NOFILE;
+    }
+    if (!S_ISSOCK(st.st_mode)) {
+        krb5_set_error_message(context, -1,
+                               "File %s exists but is"
+                               "not a socket", sockname);
+        return KRB5_CC_FORMAT;
+    }
+    return 0;
+}
+
+static const char * KRB5_CALLCONV
+rcc_get_name(krb5_context context, krb5_ccache cache)
+{
+    rcc_data *data = cache->data;
+
+    return data->residual;
+}
+
+/* Construct a cache object given a residual string and file ccache.  Take
+ * ownership of fcc on success. */
+static krb5_error_code
+make_cache(const char *residual, krb5_ccache fcc, krb5_ccache *cache_out)
+{
+    krb5_ccache cache = NULL;
+    rcc_data *data = NULL;
+    char *residual_copy = NULL;
+
+    cache = malloc(sizeof(*cache));
+    if (cache == NULL)
+        goto oom;
+    data = malloc(sizeof(*data));
+    if (data == NULL)
+        goto oom;
+    residual_copy = strdup(residual);
+    if (residual_copy == NULL)
+        goto oom;
+
+    data->residual = residual_copy;
+    data->fcc = fcc;
+    cache->ops = &krb5_rcc_ops;
+    cache->data = data;
+    cache->magic = KV5M_CCACHE;
+    *cache_out = cache;
+    return 0;
+
+oom:
+    free(cache);
+    free(data);
+    free(residual_copy);
+    return ENOMEM;
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_resolve(krb5_context context, krb5_ccache *cache_out, const char *residual)
+{
+    krb5_error_code ret;
+    krb5_ccache fcc;
+
+    /* TODO remove this */
+    printf("hello remote ccache\n");
+    exit(1);
+
+    *cache_out = NULL;
+
+    ret = krb5_cc_default(context, &fcc);
+    if (ret)
+        goto cleanup;
+    ret = make_cache(residual, fcc, cache_out);
+    if (ret)
+        krb5_fcc_ops.close(context, fcc);
+
+cleanup:
+    return ret;
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_gen_new(krb5_context context, krb5_ccache *cache_out)
+{
+    /* TODO implement remote socket */
+
+    /*
+     * Return error because we can't guess socket
+     * TODO If we decide to implement default socket,
+     * add an implementation here.
+     */
+    return -1;
+    /*
+    krb5_error_code ret;
+    krb5_ccache fcc = NULL;
+
+    *cache_out = NULL;
+    ret = krb5_fcc_ops.gen_new(context, &fcc);
+    if (ret)
+        goto cleanup;
+    ret = make_cache(residual, fcc, cache_out);
+    if (ret)
+        goto cleanup;
+    fcc = NULL;
+
+cleanup:
+    if (fcc != NULL)
+        krb5_fcc_ops.destroy(context, fcc);
+    return ret;
+    */
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_init(krb5_context context, krb5_ccache cache, krb5_principal princ)
+{
+    /* TODO implement remote socket */
+
+    rcc_data *data = cache->data;
+
+    /* TODO remove this */
+    printf("hello remote ccache\n");
+    exit(1);
+
+    return krb5_fcc_ops.init(context, data->fcc, princ);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_destroy(krb5_context context, krb5_ccache cache)
+{
+    rcc_data *data = cache->data;
+    krb5_error_code ret;
+
+    ret = krb5_fcc_ops.destroy(context, data->fcc);
+    free(data->residual);
+    free(data);
+    return ret;
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_close(krb5_context context, krb5_ccache cache)
+{
+    rcc_data *data = cache->data;
+    krb5_error_code ret;
+
+    ret = krb5_fcc_ops.close(context, data->fcc);
+    free(data->residual);
+    free(data);
+    return ret;
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_store(krb5_context context, krb5_ccache cache, krb5_creds *creds)
+{
+    rcc_data *data = cache->data;
+
+    return krb5_fcc_ops.store(context, data->fcc, creds);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_retrieve(krb5_context context, krb5_ccache cache, krb5_flags flags,
+             krb5_creds *mcreds, krb5_creds *creds)
+{
+    /* TODO implement remote socket */
+    rcc_data *data = cache->data;
+
+    return krb5_fcc_ops.retrieve(context, data->fcc, flags, mcreds,
+                                 creds);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_get_princ(krb5_context context, krb5_ccache cache,
+              krb5_principal *princ_out)
+{
+    rcc_data *data = cache->data;
+
+    return krb5_fcc_ops.get_princ(context, data->fcc, princ_out);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_get_first(krb5_context context, krb5_ccache cache, krb5_cc_cursor *cursor)
+{
+    rcc_data *data = cache->data;
+
+    return krb5_fcc_ops.get_first(context, data->fcc, cursor);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_get_next(krb5_context context, krb5_ccache cache, krb5_cc_cursor *cursor,
+             krb5_creds *creds)
+{
+    rcc_data *data = cache->data;
+
+    return krb5_fcc_ops.get_next(context, data->fcc, cursor, creds);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_end_get(krb5_context context, krb5_ccache cache, krb5_cc_cursor *cursor)
+{
+    rcc_data *data = cache->data;
+
+    return krb5_fcc_ops.end_get(context, data->fcc, cursor);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_remove_cred(krb5_context context, krb5_ccache cache, krb5_flags flags,
+                krb5_creds *creds)
+{
+    rcc_data *data = cache->data;
+
+    return krb5_fcc_ops.remove_cred(context, data->fcc, flags, creds);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_set_flags(krb5_context context, krb5_ccache cache, krb5_flags flags)
+{
+    rcc_data *data = cache->data;
+
+    return krb5_fcc_ops.set_flags(context, data->fcc, flags);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_get_flags(krb5_context context, krb5_ccache cache, krb5_flags *flags_out)
+{
+    rcc_data *data = cache->data;
+
+    return krb5_fcc_ops.get_flags(context, data->fcc, flags_out);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_ptcursor_new(krb5_context context, krb5_cc_ptcursor *cursor_out)
+{
+    // Per-type cursor acts as an fcc cursor for the purposes of rcc.
+    // TODO: Implement the ability to iterate over all available tickets?
+
+    return krb5_fcc_ops.ptcursor_new(context, cursor_out);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_ptcursor_next(krb5_context context, krb5_cc_ptcursor cursor,
+                  krb5_ccache *cache_out)
+{
+    // Per-type cursor acts as an fcc cursor for the purposes of rcc.
+    return krb5_fcc_ops.ptcursor_next(context, cursor, cache_out);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_ptcursor_free(krb5_context context, krb5_cc_ptcursor *cursor)
+{
+    // Per-type cursor acts as an fcc cursor for the purposes of rcc.
+    return krb5_fcc_ops.ptcursor_free(context, cursor);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_lastchange(krb5_context context, krb5_ccache cache,
+               krb5_timestamp *time_out)
+{
+    rcc_data *data = cache->data;
+
+    return krb5_fcc_ops.lastchange(context, data->fcc, time_out);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_lock(krb5_context context, krb5_ccache cache)
+{
+    rcc_data *data = cache->data;
+
+    return krb5_fcc_ops.lock(context, data->fcc);
+}
+
+static krb5_error_code KRB5_CALLCONV
+rcc_unlock(krb5_context context, krb5_ccache cache)
+{
+    rcc_data *data = cache->data;
+
+    return krb5_fcc_ops.unlock(context, data->fcc);
+}
+
+const krb5_cc_ops krb5_rcc_ops = {
+    0,
+    "REMOTE",
+    rcc_get_name,
+    rcc_resolve,
+    rcc_gen_new,
+    rcc_init,
+    rcc_destroy,
+    rcc_close,
+    rcc_store,
+    rcc_retrieve,
+    rcc_get_princ,
+    rcc_get_first,
+    rcc_get_next,
+    rcc_end_get,
+    rcc_remove_cred,
+    rcc_set_flags,
+    rcc_get_flags,
+    rcc_ptcursor_new,
+    rcc_ptcursor_next,
+    rcc_ptcursor_free,
+    NULL, /* move */
+    rcc_lastchange,
+    NULL, /* wasdefault */
+    rcc_lock,
+    rcc_unlock,
+    NULL, /* switch_to */
+};
