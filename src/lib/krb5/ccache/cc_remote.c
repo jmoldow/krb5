16ad75f337e8404691c6a623bf41b170d73cb0a8
David Lawrence <dlaw@dlaw.me>
2013-12-07 17:41:25 -0500

    remove unneeded parts of cc_remote.c

--- src/lib/krb5/ccache/cc_remote.c
+++ src/lib/krb5/ccache/cc_remote.c
@@ -1,29 +1,3 @@
-/* -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil -*- */
-/* lib/krb5/ccache/cc_remote.c - Remote-based credential cache collection */
-/*
- * Copyright (C) 2011 by the Massachusetts Institute of Technology.
- * All rights reserved.
- *
- * Export of this software from the United States of America may
- *   require a specific license from the United States Government.
- *   It is the responsibility of any person or organization contemplating
- *   export to obtain such a license before exporting.
- *
- * WITHIN THAT CONSTRAINT, permission to use, copy, modify, and
- * distribute this software and its documentation for any purpose and
- * without fee is hereby granted, provided that the above copyright
- * notice appear in all copies and that both that copyright notice and
- * this permission notice appear in supporting documentation, and that
- * the name of M.I.T. not be used in advertising or publicity pertaining
- * to distribution of the software without specific, written prior
- * permission.  Furthermore if you modify this software you must label
- * your software as modified software and not distribute it in such a
- * fashion that it might be confused with the original M.I.T. software.
- * M.I.T. makes no representations about the suitability of
- * this software for any purpose.  It is provided "as is" without express
- * or implied warranty.
- */
-
 #include "k5-int.h"
 #include "cc-int.h"
 
@@ -33,10 +7,6 @@
 #if HAVE_SYS_STAT_H
 #include <sys/stat.h>
 #endif
-
-/* This is Unix-only for now.  To work on Windows, we will need opendir/readdir
- * replacements and possibly more flexible newline handling. */
-#ifndef _WIN32
 
 #include <dirent.h>
 
@@ -520,5 +490,3 @@ const krb5_cc_ops krb5_rcc_ops = {
     rcc_unlock,
     rcc_switch_to,
 };
-
-#endif /* not _WIN32 */
