9ce6efa8cda671038edfa8ed35a3d5773721a642
David Lawrence <dlaw@dlaw.me>
2013-12-07 16:12:33 -0500

    kserialize is now production-ready

--- util/kserialize/kserialize.c
+++ util/kserialize/kserialize.c
@@ -1,44 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <krb5.h>
 
+#define CHECK(EXPR) if(EXPR) {printf("Error: " #EXPR "\n"); exit(1);}
+
 int main(int argc, char *argv[])
 {
-    char *service_name = argv[1];
-    char *out_file = argv[2];
-    
     krb5_context context;
-    krb5_error_code err;
-    krb5_ccache ccache;
-    krb5_ccache dummy;
-    krb5_principal me;
+    krb5_ccache ccache, dummy;
     krb5_creds in_creds, out_creds;
 
-    // TODO: check the error code returned by each krb5_ function.
+    if (argc < 3) {
+        printf("Usage: kserialize server_principal output_file\n");
+        exit(1);
+    }
+    char *service_name = argv[1];
+    char *out_file = argv[2];
 
-    krb5_init_context(&context);
-
-    krb5_cc_default(context, &ccache);
-    krb5_cc_get_principal(context, ccache, &me);
     memset(&in_creds, 0, sizeof(in_creds));
-    in_creds.client = me;
-    krb5_parse_name(context, service_name, &in_creds.server);
-    krb5_cc_retrieve_cred(context, ccache, KRB5_TC_MATCH_SRV_NAMEONLY,
-                          &in_creds, &out_creds);
+    CHECK(krb5_init_context(&context));
+
+    // Fetch the desired ticket from the system credential cache.
+    CHECK(krb5_cc_default(context, &ccache));
+    CHECK(krb5_cc_get_principal(context, ccache, &in_creds.client));
+    CHECK(krb5_parse_name(context, service_name, &in_creds.server));
+    CHECK(krb5_cc_retrieve_cred(context, ccache, KRB5_TC_MATCH_SRV_NAMEONLY,
+				&in_creds, &out_creds));
+    CHECK(krb5_cc_close(context, ccache));
+
+    // Write the ticket out to a file.
+    CHECK(krb5_cc_resolve(context, out_file, &dummy));
+    CHECK(krb5_cc_initialize(context, dummy, in_creds.client));
+    CHECK(krb5_cc_store_cred(context, dummy, &out_creds));
+    CHECK(krb5_cc_close(context, dummy));
+
+    // Clean up and exit.
+    krb5_free_principal(context, in_creds.client);
     krb5_free_principal(context, in_creds.server);
-    krb5_cc_close(context, ccache);
-
-    printf("got cred with ticket length %x\n", out_creds.ticket.length);
-    
-    krb5_cc_resolve(context, out_file, &dummy);
-    krb5_cc_initialize(context, dummy, me);
-    krb5_cc_store_cred(context, dummy, &out_creds);
-    krb5_cc_close(context, dummy);
-
-    krb5_free_principal(context, me);
     krb5_free_cred_contents(context, &out_creds);
     krb5_free_context(context);
-
     exit(0);
 }
