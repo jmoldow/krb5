f20cfa5a589040c31b7b1300e0b65406a11d83d0
David Lawrence <dlaw@dlaw.me>
2013-12-07 15:52:39 -0500

    Add utility to serialize a service ticket to a file.

---
+++ util/kserialize/kserialize.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <krb5.h>
+
+int main(int argc, char *argv[])
+{
+    char *service_name = argv[1];
+    char *out_file = argv[2];
+    
+    krb5_context context;
+    krb5_error_code err;
+    krb5_ccache ccache;
+    krb5_ccache dummy;
+    krb5_principal me;
+    krb5_creds in_creds, out_creds;
+
+    // TODO: check the error code returned by each krb5_ function.
+
+    krb5_init_context(&context);
+
+    krb5_cc_default(context, &ccache);
+    krb5_cc_get_principal(context, ccache, &me);
+    memset(&in_creds, 0, sizeof(in_creds));
+    in_creds.client = me;
+    krb5_parse_name(context, service_name, &in_creds.server);
+    krb5_cc_retrieve_cred(context, ccache, KRB5_TC_MATCH_SRV_NAMEONLY,
+                          &in_creds, &out_creds);
+    krb5_free_principal(context, in_creds.server);
+    krb5_cc_close(context, ccache);
+
+    printf("got cred with ticket length %x\n", out_creds.ticket.length);
+    
+    krb5_cc_resolve(context, out_file, &dummy);
+    krb5_cc_initialize(context, dummy, me);
+    krb5_cc_store_cred(context, dummy, &out_creds);
+    krb5_cc_close(context, dummy);
+
+    krb5_free_principal(context, me);
+    krb5_free_cred_contents(context, &out_creds);
+    krb5_free_context(context);
+
+    exit(0);
+}
