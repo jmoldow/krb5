7620ac87987d90515fadbef2a82089eb02df6290
Miriam Gershenson <mgersh@mit.edu>
2013-12-08 04:10:25 -0500

    Initial code for agent, and test script
    The agent uses kserialize; updates kserialize/kunserialize to accept ccache
    location

---
+++ util/agent_socket.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python2.7
+
+# Basic starting code for the agent
+# Set up for testing, doesn't handle errors well yet
+
+import os, os.path
+import socket
+
+# for testing/demo, can make this an argument later
+sock_path = "localhost"
+sock_port = 8001
+uid = os.getuid()
+# can implement setting a different location later if we care
+cache_location = "/tmp/krb5cc_agent_"+str(uid)
+service_ticket_location = "/tmp/service_ticket"
+
+sock = socket.socket()
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+def get_full_msg(conn):
+    """
+    Reads in a complete message from |conn|. This method handles the protocol
+    "length\nmsg" by continuing to read from the socket until we have assembled
+    a message of the appropriate length.
+    """
+    full_msg = ""
+    length = -1
+    while length != 0:
+        msg = conn.recv(1024)
+        print "recv:", msg
+        if not msg:
+            raise ValueError("No input received over socket")
+        full_msg += msg
+        if length < 0 and full_msg.find('\n') >= 0:
+            # initialize length
+            length_str, rem = full_msg.split('\n', 1)
+            full_msg = rem
+            length = int(length_str) - len(rem)
+        elif length > 0:
+            length -= len(msg)
+    return (full_msg, length)
+
+def kinit(principal):
+    return os.system("kinit -c "+cache_location+" "+principal)
+
+def get_tkt(server_principal):
+    command = "kvno -c "+cache_location+" "+server_principal
+    print command
+    return os.system(command)
+
+
+try:
+    sock.bind((sock_path, sock_port))
+    sock.listen(0)
+    while True:
+        conn, addr = sock.accept()
+
+        # read and assemble the message
+        full_msg, length = get_full_msg(conn)
+        cmd, arg = full_msg.split('\n', 1)
+        
+        if cmd == "kinit":
+            principal = arg
+            creds = kinit(principal)
+            if creds == 0:
+                conn.send("OK")
+            else:
+                conn.send("FAIL")
+        elif cmd == "ticket":
+            server_principal = arg
+            status = get_tkt(server_principal)
+            if status == 0:
+                if os.path.isfile(service_ticket_location):
+                    os.remove(service_ticket_location)
+                kserialize_cmd = "kserialize "+cache_location+" "+ \
+                    server_principal+" "+service_ticket_location
+                print kserialize_cmd
+                os.system(kserialize_cmd)
+
+                f = open(service_ticket_location, 'r')
+                tkt = f.read()
+                conn.send("%d\n%s" % (len(tkt), tkt))
+            else:
+                conn.send("FAIL")
+except (IOError, KeyboardInterrupt) as e:
+    sock.close()
+    exit()
+            
+    
+
--- util/kserialize/kunserialize.c
+++ util/kserialize/kunserialize.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <com_err.h>
 #include <krb5.h>
 
-#define CHECK(EXPR) if(EXPR) {printf("Error: " #EXPR "\n"); exit(1);}
+#define CHECK(EXPR, CTX) if(ret=(EXPR)) {com_err("kunserialize", ret, (CTX)); exit(1);}
 
 int main(int argc, char *argv[])
 {
@@ -10,26 +11,34 @@ int main(int argc, char *argv[])
     krb5_ccache ccache;
     krb5_creds creds;
     krb5_cc_cursor cursor;
+    long ret = 0;
 
-    if (argc < 2) {
-        printf("Usage: kunserialize input_file\n");
+    if (argc < 3) {
+        printf("Usage: kunserialize input_file ccache_file\n");
         exit(1);
     }
     char *in_file = argv[1];
+    char *ccache_file = argv[2];
 
-    CHECK(krb5_init_context(&context));
+    CHECK(krb5_init_context(&context), "krb5_init_context");
 
     // Read the serialized ticket from the input file.
-    CHECK(krb5_cc_resolve(context, in_file, &ccache));
-    CHECK(krb5_cc_start_seq_get(context, ccache, &cursor));
-    CHECK(krb5_cc_next_cred(context, ccache, &cursor, &creds));
-    CHECK(krb5_cc_end_seq_get(context, ccache, &cursor));
-    CHECK(krb5_cc_close(context, ccache));
+    CHECK(krb5_cc_resolve(context, in_file, &ccache), "krb5_cc_resolve");
+    CHECK(krb5_cc_start_seq_get(context, ccache, &cursor),
+          "krb5_cc_start_seq_get");
+    CHECK(krb5_cc_next_cred(context, ccache, &cursor, &creds),
+          "krb5_cc_next_cred");
+    CHECK(krb5_cc_end_seq_get(context, ccache, &cursor),
+          "krb5_cc_end_seq_get");
+    CHECK(krb5_cc_close(context, ccache),
+          "krb5_cc_close");
 
     // Write the ticket into the system credential cache.
-    CHECK(krb5_cc_default(context, &ccache));
-    CHECK(krb5_cc_store_cred(context, ccache, &creds));
-    CHECK(krb5_cc_close(context, ccache));
+    CHECK(krb5_cc_resolve(context, ccache_file, &ccache),
+          "krb5_cc_resolve");
+    CHECK(krb5_cc_store_cred(context, ccache, &creds),
+          "krb5_cc_store_cred");
+    CHECK(krb5_cc_close(context, ccache), "krb5_cc_close");
 
     // Clean up and exit.
     krb5_free_cred_contents(context, &creds);
