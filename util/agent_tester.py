d43a7aec67a6df4f6224e176ef2263d28951ec4c
Gurtej Kanwar <gurtej@mit.edu>
2013-12-08 23:44:24 -0500

    Updates the agent to kserialize; updates kserialize/kunserialize to accept ccache location

--- util/agent_tester.py
+++ util/agent_tester.py
@@ -6,7 +6,7 @@ sock_port = 8001
 sock = socket.socket()
 
 sock.connect((sock_path, sock_port))
-msg = "kinit\nmgersh"
+msg = "kinit\ngurtej@ATHENA.MIT.EDU"
 sock.send(str(len(msg))+"\n"+msg)
 print sock.recv(1024)
 sock.shutdown(socket.SHUT_RDWR)
@@ -15,6 +15,6 @@ sock.close()
 sock = socket.socket()
 
 sock.connect((sock_path, sock_port))
-msg = "ticket\nhost/linerva.mit.edu"
+msg = "ticket\nhost/linerva.mit.edu@ATHENA.MIT.EDU"
 sock.send(str(len(msg))+"\n"+msg)
 print sock.recv(1024)
