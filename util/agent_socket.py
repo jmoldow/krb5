66d37565c5eba7a3948a8db196db5c8125330243
Gurtej Kanwar <gurtej@mit.edu>
2013-12-08 22:57:37 -0500

    Makes agent_socket.py more robust, cleans up a few errors

--- util/agent_socket.py
+++ util/agent_socket.py
@@ -1,8 +1,8 @@
 # Basic starting code for the agent
 # Set up for testing, doesn't handle errors well yet
 
-import socket
 import os, os.path
+import socket
 
 # for testing/demo, can make this an argument later
 sock_path = "localhost"
@@ -10,17 +10,33 @@ sock_port = 8001
 uid = os.getuid()
 # can implement setting a different location later if we care
 cache_location = "/tmp/krb5cc_agent_"+str(uid)
+service_ticket_location = "/tmp/service_ticket"
 
 sock = socket.socket()
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-def get_rest_of_msg(conn, length, msg):
-    still_need = length -1024
-    while still_need > 0:
-        msg_continued = conn.recv(1024)
-        msg = msg + msg_continued
-        still_need = still_need - 1024
-    return msg
+def get_full_msg(conn):
+    """
+    Reads in a complete message from |conn|. This method handles the protocol
+    "length\nmsg" by continuing to read from the socket until we have assembled
+    a message of the appropriate length.
+    """
+    full_msg = ""
+    length = -1
+    while length != 0:
+        msg = conn.recv(1024)
+        print "recv:", msg
+        if not msg:
+            raise ValueError("No input received over socket")
+        full_msg += msg
+        if length < 0 and full_msg.find('\n') >= 0:
+            # initialize length
+            length_str, rem = full_msg.split('\n', 1)
+            full_msg = rem
+            length = int(length_str) - len(rem)
+        elif length > 0:
+            length -= len(msg)
+    return (full_msg, length)
 
 def kinit(principal):
     return os.system("kinit -c "+cache_location+" "+principal)
@@ -36,38 +52,42 @@ try:
     sock.listen(0)
     while True:
         conn, addr = sock.accept()
-        # hopefully big enough to get whole message in one read
-        msg = conn.recv(1024)
-        if not msg:
-            break
-        else:
-            tokens = msg.split('\n', 2)
-            if tokens[1] == "kinit":
-                # length calculation: tokens[0] = len(tokens[1])+len(principal)+2
-                # and we still need to read tokens[0]+len(tokens[0])+1-1024
-                principal = get_rest_of_msg(conn, int(tokens[0]) + \
-                                                len(tokens[0])+1, tokens[2])
-                creds = kinit(principal)
-                if creds == 0:
-                    conn.send("OK")
-                else:
-                    conn.send("FAIL")
-            elif tokens[1] == "ticket":
-                server_principal = get_rest_of_msg(conn, int(tokens[0])+ \
-                                                       len(tokens[0])+1, tokens[2])
-                status = get_tkt(server_principal)
-                if status == 0:
-                    ## For testing, change this line to point to
-                    ## your compiled kcpytkt
-                    os.system("kcpytkt -c "+cache_location+" "+ \
-                                  "/tmp/service_ticket "+server_principal)
-                                 "/tmp/service_ticket "+server_principal)
 
-                    f = open("/tmp/service_ticket", 'r')
-                    conn.send(f.read())
-                else:
-                    conn.send("FAIL")
-except IOError, KeyboardInterrupt:
+        # read and assemble the message
+        full_msg, length = get_full_msg(conn)
+        cmd, arg = full_msg.split('\n', 1)
+        
+        if cmd == "kinit":
+            principal = arg
+            creds = kinit(principal)
+            if creds == 0:
+                conn.send("OK")
+            else:
+                conn.send("FAIL")
+        elif cmd == "ticket":
+            server_principal = arg
+            status = get_tkt(server_principal)
+            if status == 0:
+                if os.path.isfile(service_ticket_location):
+                    os.remove(service_ticket_location)
+                kinit_cmd = "kinit -g -c "+service_ticket_location+ \
+                    " krbtgt@ATHENA.MIT.EDU"
+                print kinit_cmd
+                os.system(kinit_cmd)
+
+                ## For testing, change this line to point to
+                ## your compiled kcpytkt
+                cpy_cmd = "kcpytkt -c "+cache_location+" "+ \
+                              service_ticket_location+" "+server_principal
+                print cpy_cmd
+                os.system(cpy_cmd)
+
+                f = open(service_ticket_location, 'r')
+                tkt = f.read()
+                conn.send("%d\n%s" % (len(tkt), tkt))
+            else:
+                conn.send("FAIL")
+except (IOError, KeyboardInterrupt) as e:
     sock.close()
     exit()
             
