c2a22b4da927012d6d7882cb57c720d9dd1262bf
Gurtej Kanwar <gurtej@mit.edu>
2013-12-13 07:31:18 -0500

    Adds kerberos-android-ndk project as a submodule

--- util/agent_tester.py
+++ util/agent_tester.py
@@ -3,8 +3,9 @@
 import socket
 
 sock_path = "localhost"
-sock_port = 8001
+sock_port = 9000
 
+"""
 sock = socket.socket()
 
 sock.connect((sock_path, sock_port))
@@ -13,10 +14,11 @@ sock.send(str(len(msg))+"\n"+msg)
 print sock.recv(1024)
 sock.shutdown(socket.SHUT_RDWR)
 sock.close()
+"""
 
 sock = socket.socket()
 
 sock.connect((sock_path, sock_port))
 msg = "ticket\nhost/linerva.mit.edu@ATHENA.MIT.EDU"
-sock.send(str(len(msg))+"\n"+msg)
+sock.send(str(len(msg))+"\n"+msg+"END")
 print sock.recv(1024)
